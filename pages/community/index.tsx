a05505a4f499efa7ba90997a44425a7f7b6d2bde
jaska <farisgithub02@gmail.com>
2026-09-11 19:35:03 +0900

    feat: integration Header and Footer via Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,6 +1,8 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
+const Community: NextPage = () => {
     console.log("COMMUNITY COMPONENT - PAGES ROUTER");
     const [title, setTitle] = useState<string>("hello");
     return (
@@ -13,4 +15,4 @@ const Community = () => {
     );
 };
 
-export default Community;
+export default withLayoutBasic(Community);
---
+++ pages/property/detail.tsx
@@ -0,0 +1,7 @@
+import { Container, Stack } from "@mui/material";
+
+const PropertyDetail = () => {
+    return <Container>PROPERTY DETAIL</Container>;
+};
+
+export default PropertyDetail;
